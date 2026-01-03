d7a7c697390c384631865f884ff2eefd395aa20f
jaures nguembu <jauresnguembu@gmail.com>
2026-01-03 14:05:23 +0100

    Initial commit: Clean project structure for Véhicules Online

---
+++ backend/src/main/java/com/vehicules/application/VehiculesApplication.java
@@ -0,0 +1,11 @@
+package com.vehicules.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VehiculesApplication.class, args);
+    }
+}
